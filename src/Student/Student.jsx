625aef71cd49042b2820d7c1f4542afba2b0c1ab
Nimesh Bashitha <2022csc023@univ.jfn.ac.lk>
2025-09-18 15:45:17 +0530

    Update Student.jsx color hover

--- src/Student/Student.jsx
+++ src/Student/Student.jsx
@@ -261,7 +261,9 @@ const StudentDashboard = () => {
             {subjectsToRender.length === 0 && !loading && (<div className="text-center text-gray-500 py-10">No subjects match the current filters.</div>)}
             <div className="grid gap-6">
                 {subjectsToRender.map((subject) => (
-                    <div key={subject._id} className="bg-white rounded-lg shadow-lg p-6 hover:bg-[#E8E8E8]">
+                    <div key={subject._id} className="bg-white rounded-lg  p-6 hover:bg-green-50
+hover:shadow-lg
+transition-colors duration-300">
                         <div className="flex items-center justify-between mb-4">
                             <div>
                                 <h3 className="text-lg font-semibold text-green-900">{subject.name}</h3>
